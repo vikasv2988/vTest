9133b015570523cf24a18b05d6c33cdf867f1d9c
Vladimir Bilenko <vbilenko@yahoo.com>
2017-06-05 21:02:45 -0700

    added services test sample

--- src/test/java/com/rest/services/ServicesSmokeTest.java
+++ src/test/java/com/rest/services/ServicesSmokeTest.java
@@ -1,9 +1,18 @@
 package com.rest.services;
 
+import com.jayway.restassured.RestAssured;
+import org.junit.Test;
+
 /**
  * Created by vbilenko on 6/5/17.
  */
-public class ServicesSmokeTest {
+public class ServicesSmokeTest extends RestAssured{
 
+    @Test
+    public void  serviceServiceTest(){
+
+
+
+    }
 
 }
