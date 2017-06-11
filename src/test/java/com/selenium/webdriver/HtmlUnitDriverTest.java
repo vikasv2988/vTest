85f9fa2d3211b650e45d1ca6ed3c411f2e1deeef
Vladimir Bilenko <vbilenko@yahoo.com>
2017-06-10 23:18:19 -0700

    added log4j2

--- src/test/java/com/selenium/webdriver/HtmlUnitDriverTest.java
+++ src/test/java/com/selenium/webdriver/HtmlUnitDriverTest.java
@@ -1,9 +1,12 @@
 package com.selenium.webdriver;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
 import static junit.framework.Assert.assertTrue;
 
 
@@ -14,6 +17,7 @@ import static junit.framework.Assert.assertTrue;
 public class HtmlUnitDriverTest {
 
     String url = "http://localhost:8081/eMusicStore/";
+    private final static Logger logger = LogManager.getLogger(HtmlUnitDriverTest.class);
 
     @Test
     public void driverTest() {
@@ -23,8 +27,12 @@ public class HtmlUnitDriverTest {
         driver.get(url);
         assertTrue(driver.getTitle().startsWith("My Music"));
 
+        logger.info("I am info!");
+        logger.debug("I am debug");
+
         System.out.println("HtmlUnitDriver Completed!");
     }
+
 
     @Test
     public void browserTest() {
